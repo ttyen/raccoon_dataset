84c2b2803a5506734354178825a7ac8cf8cf8878
ttyen <ttyen.james@gmail.com>
2017-12-13 00:33:32 +0800

    Add files via upload

--- export_graph.sh
+++ export_graph.sh
@@ -1,2 +1,2 @@
-python ~/models/research/object_detection/export_inference_graph.py --input_type image_tensor
-#python ~/models/research/object_detection/export_inference_graph.py --input_type image_tensor --pipeline_config_path ./ssd_mobilenet_v1_pets.config --trained_checkpoint_prefix ./training/model.ckpt-66095 --output_directory ./output_inference_graph.pb
+#python ~/models/research/object_detection/export_inference_graph.py --input_type image_tensor
+python ~/models/research/object_detection/export_inference_graph.py --input_type image_tensor --pipeline_config_path ./ssd_mobilenet_v1_pets.config --trained_checkpoint_prefix ./training/model.ckpt-138519 --output_directory ./output_inference_graph.pb
